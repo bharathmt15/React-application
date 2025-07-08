86a2d5bbfb383fc8729150f110280e1be6c4fcc4
Bharat Kumar_155 <kumarbharat99928@gmail.com>
2025-07-08 13:28:13 +0530

    new componet for fruits app

---
+++ src/Components/Fruits.js
@@ -0,0 +1,73 @@
+import React from "react";
+import apple from "../Images/apple.jpg";
+import banana from "../Images/Banana.jpg";
+import mango from "../Images/mango.jpg";
+import orange from "../Images/orange.jpg";
+import pineapple from "../Images/pineapple.jpg";
+import grapes from "../Images/grapes.jpg";
+export default function Fruits() {
+    const fruits = [
+        {
+            slno: 101,
+            fruit_name: "Apple",
+            fruitImage: apple,
+            unit: "KG",
+            price: 120,
+        },
+        {
+            slno: 102,
+            fruit_name: "Banana",
+            fruitImage: banana,
+            unit: "Dozen",
+            price: 40,
+        },
+        {
+            slno: 103,
+            fruit_name: "Mango",
+            fruitImage: mango,
+            unit: "KG",
+            price: 100,
+        },
+        {
+            slno: 104,
+            fruit_name: "Orange",
+            fruitImage: orange,
+            unit: "KG",
+            price: 80,
+        },
+        {
+            slno: 105,
+            fruit_name: "Pineapple",
+            fruitImage: pineapple,
+            unit: "Pcs",
+            price: 90,
+        },
+        {
+            slno: 106,
+            fruit_name: "Grapes",
+            fruitImage: grapes,
+            unit: "KG",
+            price: 70,
+        },
+    ];
+    return (
+        <>
+            <div className="text-center my-3">
+                <h3>Fruits List Application</h3>
+                {fruits.map((fruit) => (
+                    <div key={fruit.slno} className="my-3">
+                        <h4>{fruit.fruit_name}</h4>
+                        <p>
+                            Price: ₹{fruit.price} per {fruit.unit}
+                        </p>
+                        <img
+                            src={fruit.fruitImage}
+                            alt={fruit.fruit_name}
+                            width="200px"
+                        />
+                    </div>
+                ))}
+            </div>
+        </>
+    );
+}
