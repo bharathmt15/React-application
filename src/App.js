2950f07934769ad5699f3514f1b881470a887a3e
Bharat Kumar_155 <kumarbharat99928@gmail.com>
2025-07-10 09:46:40 +0530

    updated the main ifle

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
 // import Home from "./Components/Home.js";
-import Fruits from "./Components/Fruits";
+// import Fruits from "./Components/Fruits";
+import Employee from "./Components/Employee";
 function App() {
     return (
         <>
-            <Fruits />
+            <Employee />
         </>
     );
 }
