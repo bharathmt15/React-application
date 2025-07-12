796471223ad8f42f9e75d3f72ad1b727e6f6f9d1
Bharat Kumar_155 <kumarbharat99928@gmail.com>
2025-07-12 10:50:59 +0530

    useeffect to get the exact cound value in the console

--- src/Components/Counter.js
+++ src/Components/Counter.js
@@ -1,8 +1,15 @@
-import {useState} from "react";
+import {useState, useEffect} from "react";
 export default function Counter() {
-    let [Count, setCount] = useState(1);
-    if (Count < 1) {
-        setCount(1);
+    let [Count, setCount] = useState(0);
+    if (Count < 0) {
+        setCount(0);
+    }
+    useEffect(() => {
+        console.log(Count);
+    }, [Count]);
+
+    if (Count < 0) {
+        setCount(0);
     }
     return (
         <div className="text-center my-4">
@@ -11,9 +18,28 @@ export default function Counter() {
                 <p className="h4">Counter {Count}</p>
             </div>
             <div>
-                <button onClick={() => setCount(Count + 1)}>Increase</button>
-                <button onClick={() => setCount(Count - 1)}>Decrease</button>
-                <button onClick={() => setCount(0)}>reset</button>
+                <button
+                    onClick={() => {
+                        setCount(Count + 1);
+                    }}
+                    className="btn btn-primary me-3"
+                >
+                    Increase
+                </button>
+                <button
+                    onClick={() => {
+                        setCount(Count - 1);
+                    }}
+                    className="btn btn-primary me-3"
+                >
+                    Decrease
+                </button>
+                <button
+                    onClick={() => setCount(0)}
+                    className="btn btn-primary me-3"
+                >
+                    reset
+                </button>
             </div>
         </div>
     );
