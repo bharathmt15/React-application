196e9f2423eb379a4c3227d7e2944dca81a6ef71
Bharat Kumar_155 <kumarbharat99928@gmail.com>
2025-07-15 20:56:10 +0530

    updated CSS font family for the componet

--- src/Components/UseEffect.js
+++ src/Components/UseEffect.js
@@ -17,43 +17,45 @@ export default function UseEffect() {
 
     return (
         <>
-            <h2 className="text-center my-2">{name}</h2>
-            <table className="table table-striped table-hover my-4 table-success">
-                <thead className="table-dark">
-                    <tr>
-                        <th>Id</th>
-                        <th>Name</th>
-                        <th>Username</th>
-                        <th>Email</th>
-                        <th>City</th>
-                        <th>Phone</th>
-                        <th>Actions</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {users.map((user) => (
-                        <tr key={user.id}>
-                            <td>{user.id}</td>
-                            <td>{user.name}</td>
-                            <td>{user.username}</td>
-                            <td>{user.email}</td>
-                            <td>{user.address.city}</td>
-                            <td>{user.phone}</td>
-                            <td>
-                                <button className="btn btn-primary ms-3">
-                                    Edit
-                                </button>
-                                <button
-                                    className="btn btn-danger ms-3"
-                                    onClick={() => deluser(user.id)}
-                                >
-                                    Delete
-                                </button>
-                            </td>
+            <div id="users">
+                <h2 className="text-center my-2">{name}</h2>
+                <table className="table table-striped table-hover my-4 table-success">
+                    <thead className="table-dark">
+                        <tr>
+                            <th>Id</th>
+                            <th>Name</th>
+                            <th>Username</th>
+                            <th>Email</th>
+                            <th>City</th>
+                            <th>Phone</th>
+                            <th>Actions</th>
                         </tr>
-                    ))}
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody>
+                        {users.map((user) => (
+                            <tr key={user.id}>
+                                <td>{user.id}</td>
+                                <td>{user.name}</td>
+                                <td>{user.username}</td>
+                                <td>{user.email}</td>
+                                <td>{user.address.city}</td>
+                                <td>{user.phone}</td>
+                                <td>
+                                    <button className="btn btn-primary ms-3">
+                                        Edit
+                                    </button>
+                                    <button
+                                        className="btn btn-danger ms-3"
+                                        onClick={() => deluser(user.id)}
+                                    >
+                                        Delete
+                                    </button>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
         </>
     );
 }
