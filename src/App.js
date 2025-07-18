75c70bfaa176d2f94f0f7bab0b5df3e1d78e70d9
Bharat Kumar_155 <kumarbharat99928@gmail.com>
2025-07-18 10:47:59 +0530

    new component rendering

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
-import Navbar from "./Components/Navbar/Navbar";
+// import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+// import Navbar from "./Components/Navbar/Navbar";
+import Propdrilling from "./PropsDrilling/Propdrilling";
 
 // previously used components
 // import Ecomstore from "./Components/Ecomstore";
@@ -11,28 +12,29 @@ import Navbar from "./Components/Navbar/Navbar";
 // import Employee from "./Components/Employee";
 // import Fruitsapp2 from "./Components/Fruitsapp2";
 
-import Idly from "./Components/Navbar/Idly";
-import Dosa from "./Components/Navbar/Dosa";
-import Bonda from "./Components/Navbar/Bonda";
-import Vada from "./Components/Navbar/Vada";
-import Roti from "./Components/Navbar/Chappati";
-import Puri from "./Components/Navbar/Puri";
-import Allfood from "./Components/Navbar/Allfood";
+// import Idly from "./Components/Navbar/Idly";
+// import Dosa from "./Components/Navbar/Dosa";
+// import Bonda from "./Components/Navbar/Bonda";
+// import Vada from "./Components/Navbar/Vada";
+// import Roti from "./Components/Navbar/Chappati";
+// import Puri from "./Components/Navbar/Puri";
+// import Allfood from "./Components/Navbar/Allfood";
 
 function App() {
     return (
-        <Router>
-            <Navbar />
-            <Routes>
-                <Route path="/Idly" element={<Idly />} />
-                <Route path="/Dosa" element={<Dosa />} />
-                <Route path="/Bonda" element={<Bonda />} />
-                <Route path="/Vada" element={<Vada />} />
-                <Route path="/Puri" element={<Puri />} />
-                <Route path="/Roti" element={<Roti />} />
-                <Route path="/All_tiff" element={<Allfood />} />
-            </Routes>
-        </Router>
+        // <Router>
+        //     <Navbar />
+        //     <Routes>
+        //         <Route path="/Idly" element={<Idly />} />
+        //         <Route path="/Dosa" element={<Dosa />} />
+        //         <Route path="/Bonda" element={<Bonda />} />
+        //         <Route path="/Vada" element={<Vada />} />
+        //         <Route path="/Puri" element={<Puri />} />
+        //         <Route path="/Roti" element={<Roti />} />
+        //         <Route path="/All_tiff" element={<Allfood />} />
+        //     </Routes>
+        // </Router>
+        <Propdrilling />
     );
 }
 
