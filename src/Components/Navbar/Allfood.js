efb470905fdfb14549ea3d205c38855afa265710
Bharath kumar_155 bharathmt15 <kumarbharat99928@gmail.com>
2025-07-27 11:11:21 +0530

    chagnes made in app component to render the new component and css

--- src/Components/Navbar/Allfood.js
+++ src/Components/Navbar/Allfood.js
@@ -5,14 +5,22 @@ import Bonda from "./Bonda";
 import Chappati from "./Chappati";
 import Vada from "./Vada";
 import {useEffect} from "react";
+import hotel from "./Tiffins/hotel.jpg";
 export default function Addfood() {
     useEffect(() => {
         document.title = "PUBG MOBILE";
     }, []);
     return (
         <>
+            <div id="hotelimg">
+                <img src={hotel} alt="hotel" id="hotel" />
+            </div>
+
             <div>
                 <div>
+                    <h4 className="text-center mt-4">
+                        We serve The best Food That you could exprience
+                    </h4>
                     {<Idly />}
                     {<Dosa />}
                     {<Puri />}
